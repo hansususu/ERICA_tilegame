c8882d98964d807bbac90a328edf96e2a5b36a04
Subin Han <hansubin0039@gmail.com>
2019-11-21 14:37:44 +0900

    first commit:tilegame.py

---
+++ tilegame.py
@@ -0,0 +1,19 @@
+import random
+player = []
+computer = []
+
+def make_tile():
+    color = {"red","orange", "blue", "black"}
+    number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+    deck = []
+    for i in color:
+        for j in number:
+            ap = {'color':i, 'number':j}
+            deck.append(ap)
+    ap = {'color':"colorful", 'number':"joker"}
+    deck.append(ap)
+    ap = {'color':"blackandwhite", 'number':"joker"}
+    deck.append(ap)
+    random.shuffle(deck)
+    return deck
+1
