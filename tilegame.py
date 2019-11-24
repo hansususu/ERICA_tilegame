2bbbd8aa8ee98a75920de3b8ad4317b234c1004a
Subin Han <hansubin0039@gmail.com>
2019-11-25 01:13:39 +0900

    second commit

--- tilegame.py
+++ tilegame.py
@@ -5,14 +5,30 @@ computer = []
 def make_tile():
     color = {"red","orange", "blue", "black"}
     number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
-    deck = []
+    tile = []
     for i in color:
         for j in number:
             ap = {'color':i, 'number':j}
-            deck.append(ap)
-    ap = {'color':"colorful", 'number':"joker"}
-    deck.append(ap)
-    ap = {'color':"blackandwhite", 'number':"joker"}
-    deck.append(ap)
-    random.shuffle(deck)
-    return deck
+            tile.append(ap)
+    for i in color:
+        for j in number:
+            ap = {'color':i, 'number':j}
+            tile.append(ap)
+    random.shuffle(tile)
+    return tile
+
+def dist_tile(tile):
+    for i in range(13):
+        a = random.choice(tile)
+        player.append(a)
+        tile.remove(a)
+        b = random.choice(tile)
+        computer.append(b)
+        tile.remove(b)
+
+def show_tile(player):
+    print("my tile is")
+    for tile in player:
+        print(str(tile['color']) + str(tile['number']), end = ' ')
+    print()
+
